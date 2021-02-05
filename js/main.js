c39e57798d78aa1ec50d834b830dcfb2a4cd6398
KangMoRae <77033223+KangMoRae@users.noreply.github.com>
2021-02-05 12:53:45 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var myWindow = window.open("https://kangmorae.github.io/mac", "맥 코스메틱 모바일뷰", "width=360,height=640");
+}
+
+function myFunction2() {
+  var myWindow = window.open("https://kangmorae.github.io/sealife", "부산 아쿠아리움 모바일뷰", "width=360,height=640");
+}
